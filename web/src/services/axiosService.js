630e787419f0538d4dcf02679b150b75a1ac176c
Virginia Villalobos <virginiavt98@gmail.com>
2023-05-12 12:25:12 +0200

    comment

--- web/src/services/axiosService.js
+++ web/src/services/axiosService.js
@@ -33,4 +33,4 @@ export const removeToken = axiosService.removeToken.bind(axiosService);
 
 
 export default axiosService
-//C14CAA93-D308-42AB-ABB2-FEE62205B419s
+//C14CAA93-D308-42AB-ABB2-FEE62205B419
