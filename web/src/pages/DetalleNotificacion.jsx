f11719a3beb3cfa7a854b08cf15985bbbc56dbdb
Virginia Villalobos <virginiavt98@gmail.com>
2023-06-05 10:33:53 +0200

    sends correct id, getting error on response

--- web/src/pages/DetalleNotificacion.jsx
+++ web/src/pages/DetalleNotificacion.jsx
@@ -1,13 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '@client-layout';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addBreadcrumbs } from '../../store/user/slices/breadcrumbs/breadcrumbSlice';
+import NotificacionesService from '../services/NotificacionesServices/NotificacionesService';
 
 const DetalleNotificacion = () => {
+    const dispatch = useDispatch();
+
     let idNotificacion=useParams();
     console.log(idNotificacion.id)
+
+
+
+    useEffect(()=>{
+      // Add a new breadcrumb element
+  const label = 'Nueva página';
+  dispatch(addBreadcrumbs({ label }));
+ 
+}, [dispatch,idNotificacion]);
+
+
+const [bitacora,setBitacora]=useState('')
+const[acciones,setAcciones]=useState('');
+// const[descargarAdj,setDescargarAdj]=useState('');
+const[acuse,setAcuse]=useState('');
+
+
+useEffect(() => {
+    const fetchNotificacionData = async () => {
+        console.log('useParams()',idNotificacion.id)
+        const id=idNotificacion?.id;
+        try {
+          const notificacionesService = new NotificacionesService();
+        //   const notificacionResponse = await notificacionesService.getNotificacion(id);
+          const bitacoraResponse = await notificacionesService.getBitacoraNotificaciones(id);
+          const accionesResponse = await notificacionesService.getAccionesAMostrar(id);
+        //   const descargarAdjResponse = await notificacionesService.descargarAdjunto(idNotificacion);
+          const acuseResponse = await notificacionesService.getAcuseLectura(id);
+  
+          // Set the data obtained from the responses to the component state
+          setBitacora(bitacoraResponse.data);
+          setAcciones(accionesResponse.data);
+        //   setDescargarAdj(descargarAdjResponse.data);
+          setAcuse(acuseResponse.data);
+        } catch (error) {
+          console.error('Error fetching notification data', error);
+        }
+      };
+  
+      fetchNotificacionData();
+    }, [idNotificacion]);
+
+
+
   return (
     <Layout>
       detalle notificacion 
+      <h3>Notificación º</h3>
+      <h3>Bitácora: {bitacora}</h3>
+      <h3>Acciones:{acciones}</h3>
+      <h3>Descargar Adjunto</h3>
+      <h3>Acuse de lectura:{acuse}</h3>
     </Layout>
   )
 }
--- web/src/services/NotificacionesServices/NotificacionesService.js
+++ web/src/services/NotificacionesServices/NotificacionesService.js
@@ -9,6 +9,25 @@ class NotificacionesService {
         return axiosService.axiosInstance.get( `/Notificaciones/GetNotificaciones/`)
         
     }
-}
+    getNotificacion(idNotificacion) {
+        return axiosService.axiosInstance.get(`/Notificaciones/GetNotificacion/${idNotificacion}`);
+      }
+    
+      getBitacoraNotificaciones(idNotificacion) {
+        return axiosService.axiosInstance.get(`/Notificaciones/GetBitacoraNotificaciones/${idNotificacion}`);
+      }
+    
+      getAccionesAMostrar(idNotificacion) {
+        return axiosService.axiosInstance.get(`/Notificaciones/GetAccionesAMostrar/${idNotificacion}`);
+      }
+    
+    //   descargarAdjunto(idNotificacion) {
+    //     return axiosService.axiosInstance.get(`/Notificaciones/DescargarAdjunto/${idNotificacion}`);
+    //   }
+    
+      getAcuseLectura(idNotificacion) {
+        return axiosService.axiosInstance.get(`/Notificaciones/GetAcuseLectura/${idNotificacion}`);
+      }
+    }
 
 export default NotificacionesService;
