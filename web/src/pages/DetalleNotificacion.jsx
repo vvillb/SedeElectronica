b2bb034902f0e49281c39018a622fd929bd36d32
Virginia Villalobos <virginiavt98@gmail.com>
2023-06-07 10:22:16 +0200

    nombre del acuse de lectura

--- web/src/pages/DetalleNotificacion.jsx
+++ web/src/pages/DetalleNotificacion.jsx
@@ -5,6 +5,7 @@ import { useDispatch } from 'react-redux';
 import { addBreadcrumbs } from '../../store/user/slices/breadcrumbs/breadcrumbSlice';
 import NotificacionesService from '../services/NotificacionesServices/NotificacionesService';
 import { DataGrid } from 'devextreme-react';
+import { Button as DevButton} from 'devextreme-react';
 
 const DetalleNotificacion = () => {
     const dispatch = useDispatch();
@@ -26,6 +27,8 @@ const[acciones,setAcciones]=useState('');
 // const[descargarAdj,setDescargarAdj]=useState('');
 const[acuse,setAcuse]=useState('');
 const[infoNotificacion,setInfoNotificacion]=useState('');
+const[contenidoPDF,setContenidoPDF]=useState('');
+const[nombrePdfAcuse,setNombrePdfAcuse]=useState('');
 
 
 useEffect(() => {
@@ -48,11 +51,9 @@ useEffect(() => {
           setAcuse(acuseResponse.data);
           setAcciones(accionesResponse.data);
           setInfoNotificacion(infoNotificacionResponse.data)
-          console.log('acuse',acuse)
-          console.log('bitacora',bitacoraResponse.data)
+          setContenidoPDF(acuse.contenido);
+          setNombrePdfAcuse(acuse.nombre)
           setBitacora(bitacoraResponse.data)
-          console.log('acciones',acciones)
-          console.log('info',infoNotificacion)
         } catch (error) {
           console.error('Error fetching notification data', error);
         }
@@ -61,10 +62,28 @@ useEffect(() => {
       fetchNotificacionData();
     }, [idNotificacion]);
 
-      console.log('prueba bitacora',bitacora)
+      function handleDownload() {
+        const linkSource = `data:application/pdf;base64,${contenidoPDF}`;
+        const downloadLink = document.createElement('a');
+        const fileName = `${nombrePdfAcuse}`;
+        downloadLink.href = linkSource;
+        downloadLink.download = fileName;      
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+      }
 
-
-
+      // function handleDownloadAdjunto() {
+      //   const linkSource = `data:application/pdf;base64,${contenidoPDFAdjunto}`;
+      //   const downloadLink = document.createElement('a');
+      //   const fileName = 'acuse.pdf';
+      //   downloadLink.href = linkSource;
+      //   downloadLink.download = fileName;
+      
+      //   document.body.appendChild(downloadLink);
+      //   downloadLink.click();
+      //   document.body.removeChild(downloadLink);
+      // }
 
     const columns=['id','accion','fecha','estado_Resultante']
   return (
@@ -76,9 +95,10 @@ useEffect(() => {
         columns={columns}
         showBorders={true}
       />
-      <h3>Acciones:</h3>
-      <h3>Descargar Adjunto</h3>
-      <h3>Acuse de lectura:</h3>
+      {/* <h3>Acciones:</h3> */}
+      <DevButton onClick={handleDownload}>Descargar acuse de lectura</DevButton>
+      {/* <DevButton onClick={handleDownloadAdjunto}>Descargar Adjunto</DevButton> */}
+
     </Layout>
   )
 }
