787b9d2f22fc3763c7580fc070443d17515c39fa
Virginia Villalobos <virginiavt98@gmail.com>
2023-06-08 13:49:20 +0200

    casi funciona
    la descarga del adjunto a la primera no va bien

--- web/src/pages/DetalleNotificacion.jsx
+++ web/src/pages/DetalleNotificacion.jsx
@@ -1,147 +1,109 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import Layout from '@client-layout';
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addBreadcrumbs } from '../../store/user/slices/breadcrumbs/breadcrumbSlice';
 import NotificacionesService from '../services/NotificacionesServices/NotificacionesService';
 import { DataGrid } from 'devextreme-react';
-import { Button as DevButton} from 'devextreme-react';
+import { Button as DevButton } from 'devextreme-react';
 
 const DetalleNotificacion = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  let idNotificacion = useParams();
 
-    let idNotificacion=useParams();
+  useEffect(() => {
+    // Add a new breadcrumb element
+    const label = 'Nueva página';
+    dispatch(addBreadcrumbs({ label }));
+  }, [dispatch, idNotificacion]);
 
+  const [bitacora, setBitacora] = useState('');
+  const [acciones, setAcciones] = useState('');
+  const [acuse, setAcuse] = useState(null);
+  const [infoNotificacion, setInfoNotificacion] = useState('');
+  const [contenidoPDF, setContenidoPDF] = useState('');
+  const [nombrePdfAcuse, setNombrePdfAcuse] = useState('');
+  const [idAdj, setIdAdj] = useState('');
+  const [contenidoPDFAdjunto, setContenidoPDFAdjunto] = useState('');
+  const [nombrePDFAdjunto, setNombrePDFAdjunto] = useState('');
+  const [descargarAdj, setDescargarAdj] = useState('');
 
-
-    useEffect(()=>{
-      // Add a new breadcrumb element
-  const label = 'Nueva página';
-  dispatch(addBreadcrumbs({ label }));
- 
-}, [dispatch,idNotificacion]);
-
-
-const [bitacora,setBitacora]=useState('')
-const[acciones,setAcciones]=useState('');
-const[descargarAdj,setDescargarAdj]=useState('');
-const[acuse,setAcuse]=useState('');
-const[infoNotificacion,setInfoNotificacion]=useState('');
-const[contenidoPDF,setContenidoPDF]=useState('');
-const[nombrePdfAcuse,setNombrePdfAcuse]=useState('');
-const[idAdj,setIdAdj]=useState('');
-const[contenidoPDFAdjunto,setContenidoPDFAdjunto]=useState('');
-const[nombrePDFAdjunto,setNombrePDFAdjunto]=useState('');
-
-
-
-
-useEffect(() => {
+  useEffect(() => {
     const fetchNotificacionData = async () => {
-        
-        const id=idNotificacion?.id;
-        try {
-          const notificacionesService = new NotificacionesService();
-        //   const notificacionResponse = await notificacionesService.getNotificacion(id);
-          const bitacoraResponse = await notificacionesService.getBitacoraNotificaciones(id);
-          const accionesResponse = await notificacionesService.getAccionesAMostrar(id);
-        //   
-          const acuseResponse = await notificacionesService.getAcuseLectura(id);
-          const infoNotificacionResponse=await notificacionesService.getNotificacion(id);
-  
-          // Set the data obtained from the responses to the component state
-        
-          setAcciones(accionesResponse.data);
-          console.log('accionesResponse.data',accionesResponse.data)
-        //   setDescargarAdj(descargarAdjResponse.data);
-          setAcuse(acuseResponse.data);
-          console.log('acuseResponse.data',acuseResponse.data)
-          setInfoNotificacion(infoNotificacionResponse.data)
-          console.log('infoNotificacionResponse.data',infoNotificacionResponse.data)
-          setIdAdj(infoNotificacion?.adjuntos?.length>0 ? infoNotificacion?.adjuntos[0].id: null)
-          console.log('idAdj',idAdj);
-          setContenidoPDF(acuse.contenido);
-          console.log('acuse.contenido',acuse.contenido)
-          setNombrePdfAcuse(acuse.nombre)
-          console.log('acuse.nombre',acuse.nombre)
-          setBitacora(bitacoraResponse.data);
-          console.log('bitacoraResponse.data',bitacoraResponse.data)
-        } catch (error) {
-          console.error('Error fetching notification data', error);
-        }
-      };
-      
-      fetchNotificacionData();
-    }, [idNotificacion]);
-
-
-    
-
-
-  function handleClickAdjunto() {
-    const fetchAdjunto = async () => {
-      const id = idAdj;
-      console.log('idAdj', idAdj);
+      const id = idNotificacion?.id;
       try {
         const notificacionesService = new NotificacionesService();
-        const descargarAdjResponse = await notificacionesService.descargarAdjunto(id);
-        setDescargarAdj(descargarAdjResponse.data);
-        
+        const bitacoraResponse = await notificacionesService.getBitacoraNotificaciones(id);
+        const accionesResponse = await notificacionesService.getAccionesAMostrar(id);
+        const acuseResponse = await notificacionesService.getAcuseLectura(id);
+        const infoNotificacionResponse = await notificacionesService.getNotificacion(id);
+
+        setBitacora(bitacoraResponse.data);
+        setAcciones(accionesResponse.data);
+        setAcuse(acuseResponse.data);
+        setInfoNotificacion(infoNotificacionResponse.data);
       } catch (error) {
         console.error('Error fetching notification data', error);
       }
     };
 
-    console.log('descargarAdj',descargarAdj.nombre)
-    
-    fetchAdjunto();
-    setNombrePDFAdjunto(descargarAdj.nombre);
-    setContenidoPDFAdjunto(descargarAdj.contenido)
-    handleDownloadAdjunto();
+    fetchNotificacionData();
+  }, [idNotificacion]);
+
+  useEffect(() => {
+    if (acuse) {
+      setIdAdj(infoNotificacion?.adjuntos?.length > 0 ? infoNotificacion?.adjuntos[0].id : null);
+      setContenidoPDF(acuse.contenido);
+      setNombrePdfAcuse(acuse.nombre);
+    }
+  }, [acuse, infoNotificacion]);
+
+  async function handleClickAdjunto() {
+    try {
+      const notificacionesService = new NotificacionesService();
+      const descargarAdjResponse = await notificacionesService.descargarAdjunto(idAdj);
+      setDescargarAdj(descargarAdjResponse.data);
+      setNombrePDFAdjunto(descargarAdjResponse.data.nombre);
+      setContenidoPDFAdjunto(descargarAdjResponse.data.contenido);
+      handleDownloadAdjunto();
+    } catch (error) {
+      console.error('Error fetching notification data', error);
+    }
   }
 
+  function handleDownload() {
+    const linkSource = `data:application/pdf;base64,${contenidoPDF}`;
+    const downloadLink = document.createElement('a');
+    const fileName = `${nombrePdfAcuse}`;
+    downloadLink.href = linkSource;
+    downloadLink.download = fileName;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
 
+  function handleDownloadAdjunto() {
+    const linkSource = `data:application/pdf;base64,${contenidoPDFAdjunto}`;
+    const downloadLink = document.createElement('a');
+    const fileName = `${nombrePDFAdjunto}`;
+    downloadLink.href = linkSource;
+    downloadLink.download = fileName;
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  }
 
+  const columns = ['id', 'accion', 'fecha', 'estado_Resultante'];
 
-      function handleDownload() {
-        const linkSource = `data:application/pdf;base64,${contenidoPDF}`;
-        const downloadLink = document.createElement('a');
-        const fileName = `${nombrePdfAcuse}`;
-        downloadLink.href = linkSource;
-        downloadLink.download = fileName;      
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-      }
-
-      function handleDownloadAdjunto() {
-        const linkSource = `data:application/pdf;base64,${contenidoPDFAdjunto}`;
-        const downloadLink = document.createElement('a');
-        const fileName = `${nombrePDFAdjunto}`;
-        downloadLink.href = linkSource;
-        downloadLink.download = fileName;
-      
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeChild(downloadLink);
-      }
-
-    const columns=['id','accion','fecha','estado_Resultante']
   return (
     <Layout>
-      <h1>Notificación º {idNotificacion.id}</h1>
-      <h3>Bitácora: </h3>
-        <DataGrid
-        dataSource={bitacora}
-        columns={columns}
-        showBorders={true}
-      />
-      {/* <h3>Acciones:</h3> */}
+      <h1>Notificación {idNotificacion.id}</h1>
+      <h3>Bitácora:</h3>
+      <DataGrid dataSource={bitacora} columns={columns} showBorders={true} />
       <DevButton onClick={handleDownload}>Descargar acuse de lectura</DevButton>
-      {idAdj&&(<DevButton onClick={handleClickAdjunto}>Descargar Adjunto</DevButton>)}
-
+      {idAdj && <DevButton onClick={handleClickAdjunto}>Descargar Adjunto</DevButton>}
     </Layout>
-  )
-}
+  );
+};
 
-export default DetalleNotificacion
+export default DetalleNotificacion;
