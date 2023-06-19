84d60b598a29a8bd1769c8039cada2a17b1d482c
Virginia Villalobos <virginiavt98@gmail.com>
2023-06-19 09:36:52 +0200

    loader en breadcrumbs

--- web/src/plugins/client1/components/Breadcrumbs.jsx
+++ web/src/plugins/client1/components/Breadcrumbs.jsx
@@ -4,13 +4,13 @@ import { Link, unstable_HistoryRouter } from 'react-router-dom';
 import { clearBreadcrumbs } from '../../../../store/user/slices/breadcrumbs/breadcrumbSlice';
 import futufirma from '../../../utils/futufirma';
 import { getLoginUser, setLogoutUser } from '../../../../store/user/slices/user/userSlice';
-import { Button  as DevButton } from 'devextreme-react';
+import { Button  as DevButton, LoadIndicator } from 'devextreme-react';
 
 
 
 function Breadcrumbs() {
  
-const {  autenticado, user } = useSelector((state) => state.user)
+const {  autenticado, user ,isLoading} = useSelector((state) => state.user)
 const dispatch = useDispatch();
 
 
@@ -63,7 +63,7 @@ futufirma.emisoresReconocidos = ["FUTUVER SUBCA 001","FUTUVER SUBCA 001-18","AC 
   const breadcrumbs = useSelector((state) => state.breadcrumbs);
 
   return (
-   <>
+
     <div className="flex items-center bg-gray-800 py-2 px-4">
    
       <Link to="/" className="text-gray-300 hover:text-gray-500">Ir a la página de inicio</Link>
@@ -83,31 +83,34 @@ futufirma.emisoresReconocidos = ["FUTUVER SUBCA 001","FUTUVER SUBCA 001-18","AC 
         </ol>
       </nav>
         <div className=' flex  items-center'>
-      <div >
-       <span className="text-gray-300">
-           {autenticado?`Hola, ${user?.nombre}!`:"Para acceder con futufirma debes autenticarte"}  
-                 </span>
-      </div>
-      <div className='flex items-center'>
-     {autenticado ? (
-       <div>
-        
-         <DevButton className='px-6 flex flex-end' type='default'  onClick={logout} >
-          logout
-         </DevButton>
-       </div>
-     ) : (
-       <div>
-         <DevButton className='px-6 flex flex-end' type='default' icon='user' onClick={autenticar}>
-           login
-         </DevButton>
-       </div>
-     )}
-   </div>
-         </div> 
-      </div>
-       
-         </>    
+        {isLoading?(
+            <LoadIndicator id="large-indicator" height={60} width={60} />
+          ):(
+            <div>
+            <div>
+            <span className="text-gray-300">
+                {autenticado?`Hola, ${user?.nombre}!`:"Para acceder con futufirma debes autenticarte"}  
+            </span>
+            </div>
+            <div className='flex items-center'>
+            {autenticado ? (
+              <div>              
+                <DevButton className='px-6 flex flex-end' type='default'  onClick={logout} >
+                  logout
+                </DevButton>
+              </div>
+            ) : (
+              <div>
+                <DevButton className='px-6 flex flex-end' type='default' icon='user' onClick={autenticar}>
+                  login
+                </DevButton>
+              </div>
+            )}
+           </div> 
+           </div>
+            )}       
+      </div>       
+     </div>
 
   )
 }
